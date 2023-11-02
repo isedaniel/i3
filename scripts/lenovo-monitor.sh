4bd81d56d11c74224611c13048c643d191f6f6c9
Daniel <ise.daniel@gmail.com>
2023-11-02 16:48:25 -0300

    minor changes on monitor script

--- scripts/lenovo-monitor.sh
+++ scripts/lenovo-monitor.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 if [[ $HOSTNAME == "Dani-Lenovo" ]]; then
-	exec --no-startup-id xrandr --output HDMI-1-1 --auto --right-of eDP-1 --brightness 0.95
+	xrandr --output HDMI-1-1 --auto --right-of eDP-1 --brightness 0.95
 fi
